60a03913f923d82dc77e9326299590f898c4af84
Jake Soenneker <jake@soenneker.com>
2026-02-26 08:13:08 -0600

    Enum

--- src/NotificationChannel.cs
+++ src/NotificationChannel.cs
@@ -1,11 +1,11 @@
-﻿using Intellenum;
+using Soenneker.Gen.EnumValues;
 
 namespace Soenneker.Enums.NotificationChannels;
 
 /// <summary>
 /// An enumeration of common notification channels
 /// </summary>
-[Intellenum<string>]
+[EnumValue<string>]
 public sealed partial class NotificationChannel
 {
     public static readonly NotificationChannel Errors = new(nameof(Errors));
